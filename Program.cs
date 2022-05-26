0d6c94e67cab9e984237b0fabc02b2afdd88df28
MoonPalli <crashik911@gmail.com>
2022-05-26 19:33:20 +0300

    Formatting code

--- Program.cs
+++ Program.cs
@@ -1,34 +1,37 @@
-﻿class Programm
+﻿namespace Training
 {
-    static void Main()
+    class Program
     {
-        string[] testArray = SplitString.Solution("abcde");
-
-        foreach (var item in testArray)
-            Console.WriteLine($"\"{item}\"");
-    }
-}
-
-public class SplitString
-{
-    public static string[] Solution(string str)
-    {
-        List<string> result = new List<string>();
-        string chunk = "";
-        
-        int chunkSize = 2;
-        int stringLength = str.Length;
-        
-        for (int i = 0; i < stringLength ; i += chunkSize)
+        static void Main()
         {
-            if (i + chunkSize > stringLength)
-                chunk = (str[i].ToString() + "_");
-            else
-                chunk = (str[i].ToString() + str[i + 1].ToString());
-            
-            result.Add(chunk);
-        }
+            string[] testArray = SplitString.Solution("abcde");
 
-        return result.ToArray();
+            foreach (var item in testArray)
+                Console.WriteLine($"\"{item}\"");
+        }
+    }
+
+    public class SplitString
+    {
+        public static string[] Solution(string str)
+        {
+            List<string> result = new List<string>();
+            string chunk = "";
+            
+            int chunkSize = 2;
+            int stringLength = str.Length;
+            
+            for (int i = 0; i < stringLength ; i += chunkSize)
+            {
+                if (i + chunkSize > stringLength)
+                    chunk = (str[i].ToString() + "_");
+                else
+                    chunk = (str[i].ToString() + str[i + 1].ToString());
+                
+                result.Add(chunk);
+            }
+
+            return result.ToArray();
+        }
     }
 }
