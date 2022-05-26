7406f166947871b42e793e802ce62cb958a8f9ae
Waaargh <Legion007@bk.ru>
2022-05-26 20:09:20 +0300

    var1

--- Program.cs
+++ Program.cs
@@ -1,5 +1,28 @@
 ﻿
-string dickHeadName = "Alyosha";
+void Main()
+{
+    HighAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4");
+    Console.WriteLine("pizdec");
+    Console.ReadKey();
+}
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine($"No {dickHeadName} takoy pidoruga sranaya!!(");
+
+void HighAndLow(string numbers)
+{
+    List<int> intNumbers = new List<int>();
+    string[] charArray = numbers.Split(" ", StringSplitOptions.None);
+    foreach (var i in numbers)
+    {
+        intNumbers.Add(Convert.ToInt32(numbers.Split(" ", StringSplitOptions.None)));
+    }
+    
+    int a = intNumbers.Max();
+    int b = intNumbers.Min();
+
+    Console.WriteLine(charArray);
+    Console.WriteLine(intNumbers);
+    Console.WriteLine(a + b);
+    
+}
+
+// Kata.HighAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))
