d022eb2cf96c61b1b4cf34ee4a34b62b0a347825
Waaargh <Legion007@bk.ru>
2022-05-26 17:52:53 +0300

    Kata

--- Program.cs
+++ Program.cs
@@ -1,7 +1,28 @@
-﻿public class SplitString
+﻿class Programm
+{
+    static void Main()
+    {
+        SplitString.Solution("abcde");
+    }
+}
+
+public class SplitString
 {
     public static string[] Solution(string str)
     {
-        return null;
+        int chunkSize = 2;
+        int stringLength = str.Length;
+        for (int i = 0; i < stringLength ; i += chunkSize)
+        {
+            if (i + chunkSize > stringLength)
+            {
+                chunkSize = stringLength  - i;
+                Console.Write("'" +str.Substring(i, chunkSize) + "_'");
+                break;
+            }
+            Console.Write("'" + str.Substring(i, chunkSize) + "', ");
+        }
+        Console.ReadLine();
+        return str[];
     }
 }
